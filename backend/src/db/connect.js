6c9ce7e5bdb7571c21a3760d23e16b3a6c404d6d
Quoc <phuquocfe@gmail.com>
2024-12-27 22:07:48 +0700

    change mongo

--- backend/src/db/connect.js
+++ backend/src/db/connect.js
@@ -1,8 +1,9 @@
 import mongoose from 'mongoose';
 
 const connect = async () => {
+	const uri = `mongodb+srv://${process.env.DB_USERNAME}:${process.env.DB_PASS}@cluster0.vwhdn.mongodb.net/${process.env.DB_NAME}?retryWrites=true&w=majority`;
 	try {
-		await mongoose.connect(process.env.MONGO);
+		await mongoose.connect(uri);
 		console.log('connect mongodb');
 	} catch (error) {
 		console.log('disconnect mongodb');
